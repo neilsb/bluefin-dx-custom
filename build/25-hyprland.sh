133afa61d4ea4f1d8793706b745368bf5f7599ae
Neil Burrows <neil@pawprint.co.uk>
2026-03-07 11:08:48 +0000

    Update Hyprland package source and remove packages

--- build/25-hyprland.sh
+++ build/25-hyprland.sh
@@ -52,23 +52,18 @@ echo "::group:: Install Hyprland from COPR"
 HYPRLAND_PACKAGES=(
     xdg-desktop-portal-hyprland
     hyprland
-    cliphist
     hyprland-guiutils
-    hyprland-qt-support
-    hyprqt6engine
     hyprutils
     hypridle
     hyprlock
-    hyprshot
-    waypaper
 )
 
-log_step "Installing Hyprland packages from COPR solopasha/hyprland..."
+log_step "Installing Hyprland packages from COPR mbaldessari/hyprland..."
 log_info "Packages to install: ${HYPRLAND_PACKAGES[*]}"
 
 # Install Hyprland and related packages from COPR
 # Using isolated pattern to prevent COPR from persisting
-copr_install_isolated "nett00n/hyprland" "${HYPRLAND_PACKAGES[@]}"
+copr_install_isolated "mbaldessari/hyprland" "${HYPRLAND_PACKAGES[@]}"
 
 echo "::endgroup::"
 
